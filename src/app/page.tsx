8370849979851d5521f2cbce42d5f35763936357
phucnguyen21197 <phuc.nguyen211@gmail.com>
2026-06-30 02:18:42 +0700

    01: project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+
+const X = () => {
+  return (
+    <div>
+      <Button variant="default">
+        Click me
+      </Button>
+    </div>
+  )
+};
+
+export default X;
